31c32127fefcfe4cdd374146f7502ed26f320900
Shahulhamed20 <112880824+Shahulhamed20@users.noreply.github.com>
2022-10-07 16:03:44 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,15 @@
+package helloworld.java;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		System.out.println("Enter two numbers");
+		int first = 100;
+		int second =200;
+		System.out.println(first + "  " + second);
+		int sum = first + second;
+		System.out.println("the sum is: "+sum);
+		
+	}
+
+}
