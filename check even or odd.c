54579a8a12a7da212ed36dff2fcbe436fb2a36eb
Shahulhamed20 <112880824+Shahulhamed20@users.noreply.github.com>
2022-10-15 10:33:15 +0530

    Add files via upload

---
+++ check even or odd.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+int num;
+printf("enter an integer: ");
+scanf("%d", &num);
+if(num % 2 == 0)
+    printf("%d is odd.", num);
+else
+    printf("%d is odd.",num);
+return 0;
+}
