ee31f7efe576960ffd1c4409af7d91d7664a7578
Shahulhamed20 <112880824+Shahulhamed20@users.noreply.github.com>
2022-10-28 20:33:43 +0530

    Add files via upload

---
+++ particallyopenbook/Exchangecipher.java
@@ -0,0 +1,23 @@
+package particallyopenbook18.java;
+
+import java.util.Scanner;
+
+public class Exchangecipher {
+
+	public static void main(String[] args) {
+		 String str;
+	        String exStr="";
+	        Scanner scan=new Scanner(System.in);
+	        System.out.println("Enter the string to be exchanged");
+	        str=scan.next().toUpperCase();
+	        scan.close();
+	        char ch;
+	        for(int get=0;get<str.length();get++){
+	            ch=str.charAt(get);
+	            exStr=exStr+((char)(('A'+'Z')-ch));
+	        }
+	        System.out.println("The exchanged text is "+exStr);
+
+	}
+
+}
